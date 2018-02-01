f243d6fe04177c764f31ef7b89481048a4d5ce14
Mark McCoid <MarkMccoid@gmail.com>
2018-02-01 00:00:56 -0500

    drawer styling

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,11 +1,51 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { sortByDate, sortByAmount } from '../../actions/filters';
+import { startLogout } from '../../actions/auth';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 class SideDrawer extends React.Component {
   render() {
     return (
       <View style={styles.mainContainer}>
-        <Text>Side Drawer</Text>
+        <View style={styles.optionsContainer}>
+          <View style={styles.header}>
+            <Text style={styles.headerText}>Sort Options</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={this.props.sortByDate}>
+              <Icon 
+                style={styles.icon} 
+                name={this.props.filters.sortBy === 'amount' ? 'ios-radio-button-off' : 'ios-checkmark-circle'} 
+                size={20} 
+              />
+            </TouchableOpacity>
+            <Button 
+              color={this.props.filters.sortBy === 'date' ? 'green' : 'blue'}
+              title='Sort by Date'
+              onPress={this.props.sortByDate} 
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={this.props.sortByAmount}>
+              <Icon 
+                style={styles.icon} 
+                name={this.props.filters.sortBy === 'date' ? 'ios-radio-button-off' : 'ios-checkmark-circle'} 
+                size={20} 
+              />
+            </TouchableOpacity>
+            <Button 
+              color={this.props.filters.sortBy === 'amount' ? 'green' : 'blue'}
+              title='Sort by Amount'
+              onPress={this.props.sortByAmount} 
+            />
+          </View>
+        </View>
+        <View style={styles.logoutContainer}>
+          <Button title='LOGOUT' onPress={this.props.startLogout} />
+        </View>
       </View>
     );
   }
@@ -13,9 +53,43 @@ class SideDrawer extends React.Component {
 
 const styles = StyleSheet.create({
   mainContainer: {
+    flex: 1,
     marginTop: 30,
-
+    justifyContent: 'space-between'
+  },
+  optionsContainer: {
+  },
+  logoutContainer: {
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  header: {
+    borderBottomColor: 'black',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginTop: 5,
+  },
+  headerText: {
+    fontSize: 18,
+    paddingLeft: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  icon: {
+    margin: 10,
   }
 });
 
-export default SideDrawer;
+const mapStateToProps = (state) => {
+  return {
+    filters: state.filters,
+  };
+};
+
+export default connect(mapStateToProps, {
+  sortByDate: sortByDate,
+  sortByAmount: sortByAmount,
+  startLogout: startLogout
+})(SideDrawer);
